38c664eb77d9d8865ff6e4904cf1f953c8e25c5d
Xwatson <xuwusheng@hotmail.com>
2018-03-22 23:29:56 +0800

    添加根控制器

--- MyWarServer/Controller/BaseController.cs
+++ MyWarServer/Controller/BaseController.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
 
 namespace MyWarServer.Controller
 {
-    class BaseController
+    abstract class BaseController
     {
+        RequestCode requestCode = RequestCode.None;
+        /// <summary>
+        /// 默认requestCode
+        /// </summary>
+        public RequestCode RequestCode
+        {
+            get
+            {
+                return requestCode;
+            }
+        }
+        /// <summary>
+        /// 当ActionCode未指定的默认处理方法
+        /// </summary>
+        /// <param name="data">客户端发送的数据</param>
+        /// <returns>默认方法不返回数据</returns>
+        public virtual string DefaultHandle(string data)
+        {
+            return null;
+        }
     }
 }
