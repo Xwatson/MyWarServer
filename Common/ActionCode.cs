29e2d179721480766c58432ae1ac608ab5cc8831
Xwatson <xuwusheng@hotmail.com>
2018-03-22 22:43:45 +0800

    添加动作枚举类

---
+++ Common/ActionCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    /// <summary>
+    /// 动作code
+    /// </summary>
+    public enum ActionCode
+    {
+        None, // 默认
+    }
+}
