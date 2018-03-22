b4b6957cff25e6e44f37e7fba0bfa59edc6dfc05
Xwatson <xuwusheng@hotmail.com>
2018-03-22 23:32:07 +0800

    添加默认controller

---
+++ MyWarServer/Controller/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWarServer.Controller
+{
+    /// <summary>
+    /// 当RequestCode没有指定是调用的controller
+    /// </summary>
+    class DefaultController: BaseController
+    {
+
+    }
+}
